260df5ae9bb517e6fe33f986463d52c667f6b56b
Seve Martinez <smartinez@kraken.local>
2020-12-05 14:52:54 -0700

    updated model; started dataset class

---
+++ model/data_loader.py
@@ -0,0 +1,16 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+import pandas as pd
+
+
+class ClaimsDataset(Dataset):
+    '''CMS preprocessing dataset'''
+
+    def __init__(self, csv_file):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, idx):
+        pass
--- model/net.py
+++ model/net.py
@@ -1,8 +1,5 @@
 import torch
 import torch.nn as nn
-'''
-
-'''
 
 
 class SmartNet(nn.Module):
@@ -11,9 +8,10 @@ class SmartNet(nn.Module):
     Average pooling is used.
     Args:
         n_features: the number of total features in the dataset.
-        k: the number of time series to use. Each row is one time slice
+        k: the number of time series to use. Each row is one time slice.
     '''
 
+    # pylint: disable=too-many-instance-attributes
     def __init__(self, n_features: int, k: int = 3):
         super(SmartNet, self).__init__()
         self.n_features = n_features
@@ -28,15 +26,38 @@ class SmartNet(nn.Module):
                                out_channels=32,
                                kernel_size=(self.n_features/4, self.k))  # check this.
 
-        self.LReLU = nn.LeakyReLU()
-        self.Dropout = nn.Dropout()
-        self.pool = nn.AvgPool2d(kernel_size=2)
-
-
-        # add dense
+        self.lrelu = nn.LeakyReLU()
+        self.dropout = nn.Dropout(p=0.5)
+        self.pool = nn.AvgPool2d(kernel_size=2, stride=2)
+        self.lin1 = nn.Linear(32, 5)
+        self.flatten = torch.flatten()
+        self.soft = nn.Softmax()
 
     def forward(self, x):
         '''
-        do stuff here.
+        Feed forward pass.
+        Args:
+            x: the input data.
         '''
-        pass
+        # Block 1
+        x = self.conv1(x)
+        x = self.lrelu(x)
+        x = self.pool(x)
+
+        # Block 2
+        x = self.conv2(x)
+        x = self.lrelu(x)
+        x = self.pool(x)
+
+        # Block 3
+        x = self.conv3(x)
+        x = self.LeakyReLU(x)
+        x = self.pool(x)
+
+        # Output
+        x = self.Dropout(x)
+        x = self.flatten(x)
+        x = self.lin1(x)
+        x = self.soft(x)
+
+        return x
